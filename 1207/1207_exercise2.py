f86e1c0df5596d6b7a7327eaf21068ab4607bd3b
DDDD1225 <john22395214@gmail.com>
2023-12-07 11:31:15 +0800

    class練習(運動分類)

---
+++ 1207/1207_exercise2.py
@@ -0,0 +1,53 @@
+class Sports:
+    def __init_subclass__(cls) -> None:
+        pass
+    def __init__(self,name):
+        self.name = name
+    @property
+    def sports_name(self):
+        return self.name
+    @sports_name.setter
+    def sports_name(self,value):
+        self.name = value
+        print(f'已修改為"{self.name}"')
+class WaterSports(Sports):
+    #field :場地
+    def __init__(self,name,field):
+        super().__init__(name)
+        self.field = field
+class LandSports(Sports):
+    def __init__(self,name,field):
+        super().__init__(name)
+        self.field = field
+
+if __name__ == '__main__':
+    sportA = LandSports("排球", "室外")
+    print(sportA.sports_name)
+"""
+class ball(LandSports):
+    def __init__(self,name):
+class no_ball(LandSports):
+    def __init__(self,name):
+class ocean(WaterSports):
+    def __init__(self,name):
+class swimming_pool(WaterSports):
+    def __init__(self,name):
+"""
+"""
+    # 汽車類別
+    class Car:
+        # 駕駛方法
+        def drive(self):
+            print("drive method is called.")
+        # 加速方法
+        def accelerate(self):
+            print("accelerate method is called.")
+    # 飛機類別
+    class Airplane:
+        # 駕駛方法
+        def drive(self):
+            print("drive method is called.")
+        # 飛行方法
+        def fly(self):
+            print("fly method is called.")
+"""
